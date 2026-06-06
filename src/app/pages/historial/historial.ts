1dea98e3840ea2eb33ed97ae322367ded35b65d4
Juan Jaramillo <juanfer2177@gmail.com>
2026-06-06 16:26:38 -0500

    Error al borrar directamente

--- src/app/pages/historial/historial.ts
+++ src/app/pages/historial/historial.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Orden, OrdenesService } from '../../services/ordenes';
 import { CommonModule } from '@angular/common';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-historial',
@@ -40,7 +41,39 @@ export class Historial implements OnInit {
 
   limpiarTodo() {
 
-    if (confirm('¿Seguro que deseas borrar todas las órdenes?')) {
+    /*Swal.fire({
+      title: '¿Eliminar historial?',
+      text: 'Esta acción no se puede deshacer.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sí, eliminar',
+      cancelButtonText: 'Cancelar',
+      confirmButtonColor: '#dc2626',
+      cancelButtonColor: '#6b7280',
+      background: '#2d2d2d',
+      color: '#ffffff',
+      width: '90%'
+    }).then((result) => {
+
+      if (result.isConfirmed) {
+
+        this.ordenesService.limpiarOrdenes();
+        this.historial = [];
+
+        Swal.fire({
+          title: 'Eliminada',
+          text: 'La orden fue eliminada correctamente.',
+          icon: 'success',
+          confirmButtonColor: '#16a34a',
+          background: '#2d2d2d',
+          color: '#ffffff'
+        });
+
+      }
+
+    });*/
+    const confirmar = confirm('¿Deseas eliminar el historial?');
+    if (confirmar) {
 
       this.ordenesService.limpiarOrdenes();
       this.historial = [];
--- src/app/pages/ordenes/ordenes.ts
+++ src/app/pages/ordenes/ordenes.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrdenesService, Orden } from '../../services/ordenes';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-ordenes',
@@ -29,21 +30,50 @@ export class Ordenes {
 
     this.router.navigate(['/']);
   }
-  
+
   eliminarOrden(id: number) {
 
-    const confirmar = confirm(
-      '¿Deseas eliminar esta orden?'
-    );
+    /*Swal.fire({
+      title: '¿Eliminar orden?',
+      text: 'Esta acción no se puede deshacer.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sí, eliminar',
+      cancelButtonText: 'Cancelar',
+      confirmButtonColor: '#dc2626',
+      cancelButtonColor: '#6b7280',
+      background: '#2d2d2d',
+      color: '#ffffff',
+      width: '90%'
+    }).then((result) => {
+
+      if (result.isConfirmed) {
+
+        this.ordenesService.eliminarOrden(id);
+
+        this.ordenes = this.ordenesService.obtenerOrdenes();
+
+        Swal.fire({
+          title: 'Eliminada',
+          text: 'La orden fue eliminada correctamente.',
+          icon: 'success',
+          confirmButtonColor: '#16a34a',
+          background: '#2d2d2d',
+          color: '#ffffff'
+        });
+
+      }
+
+    });*/
+    const confirmar = confirm('¿Deseas eliminar esta orden?');
 
     if (confirmar) {
 
       this.ordenesService.eliminarOrden(id);
-
-      this.ordenes =
-        this.ordenesService.obtenerOrdenes();
+      this.ordenes = this.ordenesService.obtenerOrdenes();
 
     }
 
   }
+  
 }
